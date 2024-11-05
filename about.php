2050f8705cd3366a5c4a8bf01200386b030e0d2b
Balajiram12 <balajiselvam261@gmail.com>
2024-11-05 15:03:14 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,109 @@
+<?php 
+session_start();
+error_reporting(0);
+include 'include/config.php';
+$uid=$_SESSION['uid'];
+
+if(isset($_POST['submit']))
+{ 
+$pid=$_POST['pid'];
+
+
+$sql="INSERT INTO tblbooking (package_id,userid) Values(:pid,:uid)";
+
+$query = $dbh -> prepare($sql);
+$query->bindParam(':pid',$pid,PDO::PARAM_STR);
+$query->bindParam(':uid',$uid,PDO::PARAM_STR);
+$query -> execute();
+echo "<script>alert('Package has been booked.');</script>";
+echo "<script>window.location.href='booking-history.php'</script>";
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>Gym Management System</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Ahana Yoga HTML Template">
+	<meta name="keywords" content="yoga, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/nice-select.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	
+
+	<!-- Header Section -->
+	<?php include 'include/header.php';?>
+	<!-- Header Section end -->
+
+	
+
+	                                                                              
+	<!-- Page top Section -->
+	<section class="page-top-section set-bg" data-setbg="img/bfs.png">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>Beast Fitness Studio</h2>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+	<!-- Pricing Section -->
+	<section class="pricing-section spad">
+		<div class="container">
+			<div class="section-title text-center">
+				<img src="img/icons/logo-icon.png" alt="">
+				<h2>About Us</h2>
+			</div>
+			<div class="row">
+
+				<div class="col-lg-12 col-sm-6">
+			<p>Welcome to our gym, located in the vibrant community of Karayanchavadi, Chennai! We are dedicated to providing a welcoming and motivating environment for fitness enthusiasts of all levels. Our state-of-the-art facilities include a wide range of equipment, group classes, and personalized training options designed to help you achieve your fitness goals.
+
+At our gym, we believe in the power of community and support. Our team of certified trainers is here to guide you every step of the way, whether you're just starting your fitness journey or looking to take your training to the next level. We prioritize your health and well-being, ensuring that you have access to the best resources and a friendly atmosphere.
+
+Join us and be part of a community that celebrates fitness, wellness, and personal growth!</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+
+	<!-- Footer Section -->
+	<?php include 'include/footer.php'; ?>
+	<!-- Footer Section end -->
+
+	<div class="back-to-top"><img src="img/icons/up-arrow.png" alt=""></div>
+
+	<!-- Search model end -->
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/vendor/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
